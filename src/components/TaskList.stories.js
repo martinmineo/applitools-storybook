0177dad4a3aa9059f451c50e886fb9c63b40ec94
Martin Mineo <martinmineo@gmail.com>
2022-01-25 15:32:00 -0300

    Changing componencts to see them reflected in CI Applitools

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -14,7 +14,7 @@ const Template = (args) => <TaskList {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   tasks: [
-    { id: '1', state: 'TASK_INBOX', title: 'Build a date picker' },
+    { id: '1', state: 'TASK_INBOX', title: 'CI Test - Build a date picker' },
     { id: '2', state: 'TASK_INBOX', title: 'QA dropdown' },
     { id: '3', state: 'TASK_INBOX', title: 'Write a schema for account avatar component' },
     { id: '4', state: 'TASK_INBOX', title: 'Export logo' },
